6705ed3276f41e4103adb65dc292d086ba4aad52
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-06-15 09:13:59 -0700

    Temporarily break CI test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -4,4 +4,4 @@ class HelloViewTests(TestCase):
     def test_root_returns_alive_message(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Inventory app is alive")
+        self.assertContains(response, "THIS SHOULD FAIL")
